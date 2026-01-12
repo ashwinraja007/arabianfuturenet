389ceda0d8e518cbd43d4d66b3fed42c82ff2929
Karthikentrepreneur <karthikjungleemara@gmail.com>
2026-01-12 13:27:41 +0000

    sanity

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -71,6 +71,7 @@ const ServiceCard = ({
           <AspectRatio ratio={16 / 9}>
             <img
               src={getServiceImage()}
+              src={image}
               alt={title}
               className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-105"
             />
