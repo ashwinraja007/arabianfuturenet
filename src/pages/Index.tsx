265d88ef1d42268fb8c30fb1874953dc6736b9e3
Karthikentrepreneur <karthikjungleemara@gmail.com>
2026-01-13 09:24:24 +0000

    commit

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,5 @@
 
-import { Suspense, lazy, useEffect } from 'react';
+import { Suspense, lazy } from 'react';
 import { Header } from "@/components/layout/Header";
 import { Footer } from "@/components/layout/Footer";
 import { Loader2 } from "lucide-react";
@@ -22,16 +22,6 @@ const LoadingComponent = () => (
 
 const Index = () => {
   useGeoRedirect();
-
-  useEffect(() => {
-    let link = document.querySelector("link[rel~='icon']") as HTMLLinkElement;
-    if (!link) {
-      link = document.createElement('link');
-      link.rel = 'icon';
-      document.head.appendChild(link);
-    }
-    link.href = '/favicon.ico';
-  }, []);
 
   return (
     <div className="min-h-screen flex flex-col relative">
